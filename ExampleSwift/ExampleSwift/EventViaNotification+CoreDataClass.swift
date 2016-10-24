a8363ffbe9db14f04681371932396f39f87eab9c
zee-pk <zee@predict.io>
2016-10-24 17:36:07 +0500

    re-generated core data files to resolve swift 3.0 errors

---
+++ ExampleSwift/ExampleSwift/EventViaNotification+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  EventViaNotification+CoreDataClass.swift
+//  ExampleSwift
+//
+//  Created by Zee on 24/10/2016.
+//  Copyright © 2016 Abdul Haseeb. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class EventViaNotification: NSManagedObject {
+
+}
