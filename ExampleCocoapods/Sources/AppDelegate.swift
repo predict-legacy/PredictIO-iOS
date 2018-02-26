416eacf8ce2a6a08954395375edeb07f4369eef5
Kieran Graham <me@kierangraham.com>
2018-02-26 12:47:06 +0100

    Release 5.3.0

--- ExampleCocoapods/Sources/AppDelegate.swift
+++ ExampleCocoapods/Sources/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     locationManager.requestAlwaysAuthorization()
-    locationManager.startUpdatingLocation()
+
     PredictIO.instance.start(apiKey: "") { (error) in
       switch error {
       case .invalidKey?:
--- Frameworks/PredictIO.framework/Headers/PredictIO-Swift.h
+++ Frameworks/PredictIO.framework/Headers/PredictIO-Swift.h
@@ -172,6 +172,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # define SWIFT_DEPRECATED_OBJC(Msg) SWIFT_DEPRECATED_MSG(Msg)
 #endif
 #if __has_feature(modules)
+@import AVFoundation;
 @import CoreLocation;
 @import CoreMotion;
 @import Foundation;
@@ -189,6 +190,10 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #pragma clang diagnostic ignored "-Wnullability"
 
 SWIFT_MODULE_NAMESPACE_PUSH("PredictIO")
+
+
+
+
 
 
 
